0d6f581148d07d8d0cb8e19e19ce3af7c6a2784d
Jeba-anika <anikanur98@gmail.com>
2023-07-17 12:11:47 +0600

    navbar code changed

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '@/redux/hook';
 import { removeUser } from '@/redux/features/user/userSlice';
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 
 
 export default function Navbar() {
@@ -17,7 +18,70 @@ export default function Navbar() {
           <div className='sm:text-xl text-base font-bold'>
             <Link to='/'>Book Worm</Link>
           </div>
-          <div>
+          <div className='block md:hidden'>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">Menu</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+
+                <DropdownMenuItem>
+                  <Button variant="link" asChild>
+                    <Link className='font-bold sm:text-xl text-base' to="/">Home</Link>
+                  </Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Button variant="link" asChild>
+                    <Link className='font-bold sm:text-xl text-base' to="/allBooks">All Books</Link>
+                  </Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Button variant="link" asChild>
+                    <Link className='font-bold sm:text-xl text-base' to="/">Home</Link>
+                  </Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Button variant="link" asChild>
+                    <Link className='font-bold sm:text-xl text-base' to="/">Home</Link>
+                  </Button>
+                </DropdownMenuItem>
+
+
+
+                {
+                  !email && <>
+                    <DropdownMenuItem>
+                      <Button variant="link" asChild>
+                        <Link className='font-bold sm:text-xl text-base' to="/signup">SignUp</Link>
+                      </Button>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Button variant="link" asChild>
+                        <Link className='font-bold sm:text-xl text-base' to="/signin">SignIn</Link>
+                      </Button>
+                    </DropdownMenuItem>
+
+                  </>
+                }
+                {email && <>
+                  <DropdownMenuItem>
+                    <Button variant="link" asChild>
+                      <Link className='font-bold sm:text-xl text-base' to="/wishlist">Wishlist</Link>
+                    </Button>
+                  </DropdownMenuItem>
+                </>}
+                {email && <>
+                  <DropdownMenuItem>
+                    <Button onClick={() => handleLogout()}>
+                      LogOut
+                    </Button>
+                  </DropdownMenuItem>
+                </>}
+
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <div className='hidden sm:hidden md:block'>
             <ul className="flex items-center ">
               <li>
                 <Button variant="link" asChild>
@@ -58,6 +122,6 @@ export default function Navbar() {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,23 @@
 import BookCard from "@/components/bookCard";
+import Loader from "@/components/loader";
 import { useGetTopBooksQuery } from "@/redux/features/book/bookApi";
 import { IBook } from "@/types/globalTypes";
 
 export default function Home() {
-    const { data } = useGetTopBooksQuery<any>(undefined)
+    const { data, isLoading } = useGetTopBooksQuery<any>(undefined, { refetchOnMountOrArgChange: true })
     return (
-        <div className=" grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
+        <>
             {
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                data?.data?.map((book: IBook) => <BookCard book={book} handleFinishedReading={undefined}></BookCard>)
-            }
+                !isLoading ? <div className=" grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
+                    {
+                        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                        data?.data?.map((book: IBook) => <BookCard book={book} handleFinishedReading={undefined}></BookCard>)
+                    }
 
-        </div>
+                </div> :
+                    <div className="w-full h-full flex justify-center"><Loader></Loader>
+                    </div>
+            }
+        </>
     )
 }
